6dbc610b478d59f6f0091d8c1e85ac5f91e74ddf
Henrik Aronsson <arnssn@gmail.com>
2015-05-18 21:01:32 +0200

    first commit

---
+++ FSharpAdventures/Program.fs
@@ -0,0 +1,32 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+open System
+
+// function
+let checkNumber guess correctNumber =
+  if guess = correctNumber then "Win" 
+  elif guess < correctNumber then "Guess Higher" 
+  else "Guess lower"
+
+// function
+let generateNumber =
+    let rnd = System.Random()
+    rnd.Next(0,100)
+
+let double x = 
+    x * 2
+
+// Higher order function
+let quadruple x =
+    2 * double x
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d" (quadruple 2)
+    let correctNumber = generateNumber
+    printfn "Answer: %d \nGuess the number (0-100)" correctNumber
+    let guess = Console.ReadLine() |> System.Int32.Parse
+    printfn "You guessed: %A \n%s" guess (checkNumber guess correctNumber)
+    let pauseProgram = Console.ReadLine()
+    0 // return an integer exit code
