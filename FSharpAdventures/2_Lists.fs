001f888d7d6b7e735d7320a93dbe5422b5fb17df
Henrik Aronsson <arnssn@gmail.com>
2015-05-24 19:59:52 +0200

    Lists

--- FSharpAdventures/2_Lists.fs
+++ FSharpAdventures/2_Lists.fs
@@ -1,6 +1,28 @@
 ﻿module _2_Lists
 
-let numbers = [1; 2; 3; 4]
-
+open System
+ 
 let Test2 = 
     printfn "Test2"
+    let msgs = [ "Hello"; "World!" ]
+    (msgs @ ["DUDE"; "!"]) |> Seq.iter (printf "%s ")
+ 
+    printfn "\nList matching"
+ 
+    let even x =
+        match x with
+        | x when x%2 = 0 -> printfn "%d is even" x
+        | _ -> printfn "%d is odd" x
+ 
+    let evenNoPrint x =
+        match x with
+        | x when x%2 = 0 -> "is even"
+        | _ -> "is odd"
+ 
+    let printa x =
+        printfn "%d %s" x (evenNoPrint x)
+ 
+    let alist = [1 .. 6]
+    List.iter printa alist
+ 
+    List.iter (fun i -> (printfn "%d %s" i (evenNoPrint i))) alist
